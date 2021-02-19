c70a4969eba477c73790555921c3d3411c0622ff
Zhangpengyu <18746442232@163.com>
2021-02-19 11:05:58 +0800

    备案号

--- Dawn/footer.php
+++ Dawn/footer.php
@@ -3,7 +3,7 @@
 <div class="footer">
     <div class="layui-col-md12 t-copy">
         <span class="layui-breadcrumb">
-            <span>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a> <a href="http://creativecommons.org/licenses/by-nc/4.0/deed.zh"><i class="icon-cc"></i>CC-BY-NC-4.0</a></span>
+            <span>&copy; <?php echo date('Y'); ?> <a href="http://beian.miit.gov.cn">黑ICP备18002320号</a> <a href="http://creativecommons.org/licenses/by-nc/4.0/deed.zh"><i class="icon-cc"></i>CC-BY-NC-4.0</a></span>
             <span class="layui-hide-xs">Poweed by <a href="http://typecho.org/" target="_blank" rel="nofollow">Typecho</a></span>
             <span>Theme by <a href="https://www.echo.so" target="_blank">Echo</a></span>
             <span>remould by <a href="https://www.dawninshadow.com" target="_blank">TOKDawn</a></span>
